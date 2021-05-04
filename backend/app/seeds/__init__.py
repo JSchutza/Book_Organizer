4150b91c999e1c5cc252f7347e3b1b666405652e
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-05-04 16:25:02 -0500

    private character seeder completed

--- backend/app/seeds/__init__.py
+++ backend/app/seeds/__init__.py
@@ -2,7 +2,7 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .pub_chars import seed_pub_chars, undo_pub_chars
 from .books import  seed_books, undo_books
-# from .pri_chars import seed_pri_chars, undo_pri_chars
+from .pri_chars import seed_pri_chars, undo_pri_chars
 # from .pages import  seed_pages, undo_pages
 
 
@@ -18,7 +18,7 @@ def seed():
     seed_users()
     seed_pub_chars()
     seed_books()
-    # seed_pri_chars()
+    seed_pri_chars()
     # seed_pages()
 
 # Creates the `flask seed undo` command
@@ -27,5 +27,5 @@ def undo():
     undo_users()
     undo_pub_chars()
     undo_books()
-    # undo_pri_chars()
+    undo_pri_chars()
     # undo_pages()
---
+++ backend/app/seeds/pri_chars.py
@@ -0,0 +1,52 @@
+
+
+from app.models import db, PrivateCharacter
+from faker import Faker
+from random import randint
+
+fake = Faker()
+
+
+def seed_pri_chars():
+  each_icon = [
+    'https://i.imgur.com/edA9qh5.jpg',
+    'https://i.imgur.com/SrbOBhN.jpg',
+    'https://i.imgur.com/X1o1dJB.jpg',
+    'https://i.imgur.com/3h2t2jn.jpg',
+    'https://i.imgur.com/dnpLSDb.jpg',
+    'https://i.imgur.com/4G0Bd3J.jpg',
+    'https://i.imgur.com/tNHmQDx.jpg',
+    'https://i.imgur.com/5vK4RsC.jpg',
+    'https://i.imgur.com/mgB9BK3.jpg',
+    'https://i.imgur.com/xQZZT8t.jpg',
+    'https://i.imgur.com/YKwotMw.jpg',
+    'https://i.imgur.com/5exlOWk.jpg',
+    'https://i.imgur.com/C77lYz4.jpg',
+    'https://i.imgur.com/cFDZIbv.jpg',
+    'https://i.imgur.com/z24jb4a.jpg',
+    'https://i.imgur.com/auUXIPA.jpg',
+    'https://i.imgur.com/UC1WhT5.jpg',
+    'https://i.imgur.com/kAxbTFI.jpg',
+    'https://i.imgur.com/MLEpcTh.jpg',
+    'https://i.imgur.com/jX3CHRh.jpg',
+    'https://i.imgur.com/4Bo2Kiz.jpg',
+    'https://i.imgur.com/DwHPXWl.jpg',
+    'https://i.imgur.com/OFHXRI4.jpg',
+  ]
+
+  # count = 1000
+  # count -= 1
+  result = []
+  for each in each_icon:
+    result.append(PrivateCharacter(avatar=each, character_name=fake.name(), character_label="default label here", book_id=randint(1, 100)))
+
+  for character in result:
+    db.session.add(character)
+    db.session.commit()
+
+
+
+
+def undo_pri_chars():
+  db.session.execute('TRUNCATE private_characters RESTART IDENTITY CASCADE;')
+  db.session.commit()
