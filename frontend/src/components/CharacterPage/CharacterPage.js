d774daff3bcc07c1f9a13f4e66e7c772e979cb2a
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-05-04 20:11:42 -0500

    basic char page done

--- frontend/src/components/CharacterPage/CharacterPage.js
+++ frontend/src/components/CharacterPage/CharacterPage.js
@@ -1,10 +1,22 @@
 
-import React from 'react';
-import { useSelector } from "react-redux"
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from "react-redux"
+import { thunk_getAllCharacters } from "../../store/thunks/characters.js";
 
 
 const CharacterPage = () => {
   const allChars = useSelector((store) => store.characterPageReducer.characters)
+  const dispatch = useDispatch();
+
+
+
+  useEffect(() => {
+    dispatch(thunk_getAllCharacters());
+  }, []);
+
+
+
+
 
   if(allChars === null) {
     return (
@@ -14,14 +26,37 @@ const CharacterPage = () => {
     )
   }
 
+
   return (
     <>
     <div>
       <h1> Characters </h1>
+    <div>
+    {Object.values(allChars).map(eachChar => (
+      <>
+      <a>
+        <li key={eachChar.id}>
+          By: {eachChar.username}
+          <br/>
+          {eachChar.character_name}
+          <br/>
+          {eachChar.character_label}
+        </li>
+        <img src={eachChar.avatar}/>
+      </a>
+      </>
+      ))}
+      </div>
+
+
     </div>
     </>
-
   )
+
+
+
+
+
 }
 
 
