6d1efbe8eee1eeb4f310a84d60ee8f15280c0c8b
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-05-07 17:28:33 -0500

    fixed state issue when deleting pub chars

--- frontend/src/store/reducers/characters.js
+++ frontend/src/store/reducers/characters.js
@@ -10,8 +10,6 @@ const characterPageReducer = (state = { characters: null }, action) => {
   switch (action.type) {
     case ALL_CHARACTERS:
       return { ...action.characters };
-    case DELETE_USERS_PUB_CHARS:
-      return state;
     default:
       return state;
   }
@@ -28,6 +26,10 @@ const searchCharacterPageReducer = (state = { characters: null }, action) => {
       return { ...action.characters.public_characters };
     case CLEAR_SEARCH_PUB_CHARS:
       return { characters: null };
+    case DELETE_USERS_PUB_CHARS:
+      const id = action.character;
+      delete state[id];
+      return { ...state};
     default:
       return state;
   }
