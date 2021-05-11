01442f4a11cac533ed9ac899df02bd8f935360d4
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-05-11 11:45:50 -0500

    tested / implemented temp fix for signup and generating a unique search_id

--- app/hash/helper.py
+++ app/hash/helper.py
@@ -2,6 +2,7 @@ from random import randint
 
 
 
+
 def gen_search_id(the_input):
   number = 0
   for each in the_input:
@@ -12,4 +13,5 @@ def gen_search_id(the_input):
       continue
 
   result = str(the_input[0]) + str(number)
+
   return result
