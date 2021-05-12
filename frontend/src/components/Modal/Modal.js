79f813031deb153e8aa62a92a5cd57bd57637299
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-05-12 11:57:20 -0500

    the modal was the worst decicion i have ever made...

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -69,7 +69,9 @@ const Modal = ({ user }) =>  {
 
 
         {content === "CreatePubChar" ?
+        <div className={styles.create_pub_char_form}>
           <CreateCharacterForm />
+        </div>
         :
           <></>
         }
