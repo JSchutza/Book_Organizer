5053f690e1f1e129ab65bde029c2661e1135fcc2
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-09-16 10:52:12 -0700

    rewriting delete pub char button

--- frontend/src/components/DeletePubCharButton/DeletePubCharButton.js
+++ frontend/src/components/DeletePubCharButton/DeletePubCharButton.js
@@ -1,8 +1,8 @@
 
-import React from 'react';
+
 import { useDispatch } from 'react-redux';
-import { thunk_deleteUsersPubChars, thunk_getAllCharacters, thunk_searchForUsersPubChars } from "../../store/thunks/characters.js";
-import { hideModal } from "../../store/actions/modal.js";
+import { thunk_deleteUsersPubChars } from "../../store/thunks/characters.js";
+
 import { useHistory } from "react-router-dom";
 
 
@@ -18,28 +18,12 @@ const DeletePubCharButton = ({ charId, search_id, data }) => {
 
   const handle = (event, choice) => {
     event.preventDefault();
-    if (data === null) return;
-    if (choice === true) {
-      if (data.charPage === true) {
-        dispatch(thunk_deleteUsersPubChars({ charPage: true, characterId: charId, search_id: undefined }));
-        dispatch(hideModal());
-        history.push(data.lastpage);
-      } else if (data.charPage === undefined) {
-        dispatch(thunk_deleteUsersPubChars({ charPage: false, characterId: charId, search_id }));
-        dispatch(hideModal());
-        history.push(data.lastpage);
-      }
-        if (data.setIsHidden) {
-          dispatch(hideModal());
-          data.setIsHidden("");
-          history.push(data.lastpage);
-        }
-    }
-      if (data.setIsHidden) {
-        dispatch(hideModal());
-        data.setIsHidden("");
-        history.push(data.lastpage);
-      }
+
+    // dispatch(thunk_deleteUsersPubChars({ charPage: true, characterId: charId, search_id: undefined }));
+
+
+    // dispatch(thunk_deleteUsersPubChars({ charPage: false, characterId: charId, search_id }));
+
   }
 
 
