4193a9aa42e603b40c84e7dd20c1171c1bfa55a6
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-09-18 11:05:57 -0700

    SpecificPubChar component done

--- frontend/src/components/CharacterPage/CharacterPage.js
+++ frontend/src/components/CharacterPage/CharacterPage.js
@@ -8,6 +8,7 @@ import { useUser } from "../../context/UserContext.js";
 import { RiDeleteBinFill } from "react-icons/ri";
 import { GrUpdate } from "react-icons/gr";
 import { BsFillPlusSquareFill } from "react-icons/bs";
+import SpecificPubChar from "../SpecificPubChar";
 import CharacterSearch from "../CharacterSearch";
 import CreateCharacterForm from "../CreateCharacterForm";
 import LoadScreen from "../LoadScreen";
@@ -28,7 +29,7 @@ import styles from "./characterpage.module.css"
 const CharacterPage = () => {
   const [ loading, setLoading ] = useState(true);
   const [ specificChar, setSpecificChar ] = useState(false);
-  const [ charId, setCharId ] = useState(false);
+  const [ char, setChar ] = useState(false);
   const [ openModal, setOpenModal ] = useState(false);
   const [ update, setUpdate ] = useState(false);
   const [ updatePayload, setUpdatePayload ] = useState(null);
@@ -77,9 +78,9 @@ const CharacterPage = () => {
 
 
 
-  const showSpecificChar = (event, the_char_id) => {
+  const showSpecificChar = (event, charId) => {
     event.preventDefault();
-    setCharId(the_char_id);
+    setChar(allChars[charId]);
     setSpecificChar(true);
   }
 
@@ -88,7 +89,7 @@ const CharacterPage = () => {
 
   const hideSpecificChar = (event) => {
     event.preventDefault();
-    setCharId(false);
+    setChar(false);
     setSpecificChar(false);
   }
 
@@ -128,24 +129,6 @@ const CharacterPage = () => {
 
 
 
-  const toProfile = event => {
-    event.preventDefault();
-    history.push('/profile');
-  }
-
-
-
-
-
-
-
-  const handleFollowOrUnfollow = (event, userId) => {
-    event.preventDefault();
-    dispatch(thunk_followOrUnfollow(userId));
-  }
-
-
-
 
 // loading screen if the character data is not available
   if (allChars === null || loading) {
@@ -166,86 +149,11 @@ const CharacterPage = () => {
   if(specificChar){
     return (
       <>
-      <div className={styles.specific_char_wrap}>
-          {/* if the selected character belongs to the currently logged-in user */}
-          {allChars[charId].user_id === isUser.id ?
-          <>
-            <div>
-              <a href='/' onClick={event => toProfile(event)}> Profile </a>
-            </div>
-
-
-              <div className={styles.specific_char_buttons_wrap}>
-                <div className={styles.specific_char_update_button}>
-                  <ToolTip content={'Update'} >
-                    <a href='/' onClick={event => handleUpdate(event, {
-                      charId: allChars[charId].id,
-                      avatar: allChars[charId].avatar,
-                      character_label: allChars[charId].character_label,
-                      character_name: allChars[charId].character_name,
-                      created_at: allChars[charId].created_at,
-                      pub_date: allChars[charId].pub_date,
-                      user_id: allChars[charId].user_id,
-                      username: allChars[charId].username,
-                      search_id: allChars[charId].search_id
-                    })}> <GrUpdate /> </a>
-                  </ToolTip>
-                </div>
-
-                <div className={styles.specific_char_delete_button}>
-                  <ToolTip content={'Delete'} >
-                    <a href='/' onClick={event => handleDelete(event, { charId: allChars[charId].id })} > <RiDeleteBinFill /> </a>
-                  </ToolTip>
-                </div>
-            </div>
-          </>
-
-          :
-
-          <>
-          <div className={styles.specific_char_userprofile_button}>
-            <NavLink to={`/user/${allChars[charId].search_id}`} exact>
-              Profile
-            </NavLink>
-          </div>
-
-                {followingInfo.following[allChars[charId].user_id] ?
-                  <div className={styles.specific_char_follow_unfollow_button}>
-                    <a href='/' onClick={event => handleFollowOrUnfollow(event, allChars[charId].user_id)}>
-                      Unfollow
-                    </a>
-                  </div>
-
-                  :
-
-                <div className={styles.specific_char_follow_unfollow_button}>
-                    <a href='/' onClick={event => handleFollowOrUnfollow(event, allChars[charId].user_id)}>
-                      Follow
-                    </a>
-                  </div>
-                }
-        </>
-        }
-
-
-      <div className={styles.specific_char_containter}>
-          <a href='/' onClick={(event) => hideSpecificChar(event) }>
-
-            <div className={styles.specific_char_username}>
-              <h1> {allChars[charId].username} </h1>
-            </div>
-
-            <li key={charId} >
-              {allChars[charId].character_name}
-              <br/>
-              {allChars[charId].character_label}
-            </li>
-
-            <img src={allChars[charId].avatar} alt={allChars[charId].character_name} />
-          </a>
-      </div>
-
-      </div>
+        <SpecificPubChar
+          theChar={char}
+          followingInfo={followingInfo}
+          hideSpecificChar={hideSpecificChar}
+        />
       </>
       )
   }
--- frontend/src/components/SpecificPubChar/SpecificPubChar.js
+++ frontend/src/components/SpecificPubChar/SpecificPubChar.js
@@ -1,3 +1,35 @@
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory, NavLink } from "react-router-dom";
+
+
+import { RiDeleteBinFill } from "react-icons/ri";
+import { GrUpdate } from "react-icons/gr";
+
+
+import { thunk_deleteUsersPubChars } from "../../store/thunks/characters.js";
+import { thunk_getFollowing, thunk_followOrUnfollow } from "../../store/thunks/following.js";
+import { useUser } from "../../context/UserContext";
+
+
+
+import ToolTip from "../ToolTip";
+
+
+import styles from "./specificpubchar.module.css"
+
+
+
+
+const SpecificPubChar = ({ theChar=false, followingInfo=false, hideSpecificChar }) => {
+  const [update, setUpdate] = useState(false);
+  const [updatePayload, setUpdatePayload] = useState(null);
+
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { isUser } = useUser();
+
+// need to on first render / if page is reloaded get specific char information
 
 
 
@@ -6,15 +38,149 @@
 
 
 
-const SpecificPubChar = () => {
+
+  const handleFollowOrUnfollow = (event, userId) => {
+    event.preventDefault();
+    dispatch(thunk_followOrUnfollow(userId));
+  }
+
+
+
+
+
+  const toProfile = event => {
+    event.preventDefault();
+    history.push('/profile');
+  }
+
+
+
+
+  const handleUpdate = (event, payload) => {
+    event.preventDefault();
+    setUpdatePayload(payload);
+    setUpdate(true);
+  }
+
+
+
+
+  const handleDelete = (event, { charId }) => {
+    event.preventDefault();
+    dispatch(thunk_deleteUsersPubChars(charId));
+
+  };
+
+
+
+
+
+
+
+  // if theChar is false show loading screen
+  if (!theChar) {
+    return (
+    <>
+      <h1>Loading character information ... </h1>
+    </>
+    );
+  }
+
+
 
 
 
   return (
     <>
+      <div className={styles.specific_char_wrap}>
+        {/* if the selected character belongs to the currently logged-in user */}
+        {theChar.user_id === isUser.id ?
+          <>
+            <div>
+              <a href='/' onClick={event => toProfile(event)}> Profile </a>
+            </div>
+
+
+            <div className={styles.specific_char_buttons_wrap}>
+              <div className={styles.specific_char_update_button}>
+                <ToolTip content={'Update'} >
+                  <a href='/' onClick={event => handleUpdate(event, {
+                    charId: theChar.id,
+                    avatar: theChar.avatar,
+                    character_label: theChar.character_label,
+                    character_name: theChar.character_name,
+                    created_at: theChar.created_at,
+                    pub_date: theChar.pub_date,
+                    user_id: theChar.user_id,
+                    username: theChar.username,
+                    search_id: theChar.search_id
+                  })}> <GrUpdate /> </a>
+                </ToolTip>
+              </div>
+
+              <div className={styles.specific_char_delete_button}>
+                <ToolTip content={'Delete'} >
+                  <a href='/' onClick={event => handleDelete(event, { charId: theChar.id })} > <RiDeleteBinFill /> </a>
+                </ToolTip>
+              </div>
+            </div>
+          </>
+
+          :
+
+          <>
+            <div className={styles.specific_char_userprofile_button}>
+              <NavLink to={`/user/${theChar.search_id}`} exact>
+                Profile
+              </NavLink>
+            </div>
+
+            {followingInfo.following[theChar.user_id] ?
+              <div className={styles.specific_char_follow_unfollow_button}>
+                <a href='/' onClick={event => handleFollowOrUnfollow(event, theChar.user_id)}>
+                  Unfollow
+                </a>
+              </div>
+
+              :
+
+              <div className={styles.specific_char_follow_unfollow_button}>
+                <a href='/' onClick={event => handleFollowOrUnfollow(event, theChar.user_id)}>
+                  Follow
+                </a>
+              </div>
+            }
+          </>
+        }
+
+
+
+
+
+
+
+
+        <div className={styles.specific_char_containter}>
+          <a href='/' onClick={(event) => hideSpecificChar(event)}>
+
+            <div className={styles.specific_char_username}>
+              <h1> {theChar.username} </h1>
+            </div>
+
+            <li>
+              {theChar.character_name}
+              <br />
+              {theChar.character_label}
+            </li>
+
+            <img src={theChar.avatar} alt={theChar.character_name} />
+          </a>
+        </div>
+
+      </div>
     </>
   );
-  
+
 };
 
 export default SpecificPubChar;
