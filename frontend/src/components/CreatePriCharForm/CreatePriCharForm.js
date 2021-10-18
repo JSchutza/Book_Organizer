04fe70a3734dab5c263985853c635de1f711c328
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-10-18 12:46:52 -0700

    create-pri-char-modal-and-mg-preview-modal-done

--- frontend/src/components/CreatePriCharForm/CreatePriCharForm.js
+++ frontend/src/components/CreatePriCharForm/CreatePriCharForm.js
@@ -1,13 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useHistory } from "react-router-dom";
 
 import { thunk_updatePriChar, thunk_createPriChar } from "../../store/thunks/books.js";
 import { processFile } from "../../services/protectedFileUpload.js";
 
-import { nanoid } from "nanoid";
+
 
 import Errors from "../Errors";
+import ImgPreview from "../ImgPreview";
+
 
 import styles from "./createpricharform.module.css";
 
@@ -16,18 +17,13 @@ const defaultValues = { charId: '', avatar: '', character_name: '', character_la
 
 const CreatePriCharForm = ({ bookId, update=false, payload=defaultValues, closeModal }) => {
   const { charId, avatar, character_name, character_label, book_id } = payload;
-  const [avatarUrl, setAvatarUrl] = useState(avatar);
-  const [charname, setCharname] = useState(character_name);
-  const [charlabel, setCharlabel] = useState(character_label);
-  const [urlpreview, setUrlPreview] = useState(null);
+  const [ imgModal, setImgModal ] = useState(false);
+  const [ avatarUrl, setAvatarUrl ] = useState(avatar);
+  const [ charname, setCharname ] = useState(character_name);
+  const [ charlabel, setCharlabel ] = useState(character_label);
+  const [ urlpreview, setUrlPreview ] = useState(null);
 
   const dispatch = useDispatch();
-
-
-
-
-
-
 
 
 
@@ -49,9 +45,14 @@ const CreatePriCharForm = ({ bookId, update=false, payload=defaultValues, closeM
     if (result) {
         setUrlPreview(result);
         setAvatarUrl(URL.createObjectURL(result));
+      // open the img modal
+      setImgModal(true);
+
     } else {
       setUrlPreview(null);
       setAvatarUrl('');
+      // close the img modal
+      setImgModal(false);
     }
 
   };
@@ -64,6 +65,8 @@ const CreatePriCharForm = ({ bookId, update=false, payload=defaultValues, closeM
   const cancelImgChoice = () => {
     setUrlPreview(null);
     setAvatarUrl('');
+    // close the img modal
+    setImgModal(false);
   }
 
 
@@ -143,21 +146,14 @@ const CreatePriCharForm = ({ bookId, update=false, payload=defaultValues, closeM
   return (
     <>
       <Errors />
+      <ImgPreview
+        urlpreview={urlpreview}
+        cancelImgChoice={cancelImgChoice}
+        avatarUrl={avatarUrl}
+        openModal={imgModal}
+        setOpenModal={setImgModal}
+      />
 
-      {/* for previewing the image before it is sent to backend */}
-      <div className={styles.url_preview_wrap}>
-        {urlpreview === null ?
-          <></>
-          :
-          <>
-            <img src={avatarUrl} alt={"cool"}/>
-            <button onClick={cancelImgChoice}> Cancel </button>
-          </>
-        }
-      </div>
-
-
-      <div>
         <form className={styles.create_char_container} onSubmit={onSubmit}>
 
           <label>Pick an Avatar</label>
@@ -184,8 +180,6 @@ const CreatePriCharForm = ({ bookId, update=false, payload=defaultValues, closeM
           <button type='submit'> Create </button>
 
         </form>
-
-      </div>
     </>
   )
 };
