fdb773ea44cb74315fc4c2e21a928c198141abcf
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-10-20 09:23:55 -0700

    basic-social-links-added

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 
 import { VscGithub } from "react-icons/vsc";
+import { FaLinkedin } from "react-icons/fa";
 
 import styles from './footer.module.css';
 
@@ -9,6 +10,7 @@ const Footer = () => {
   return (
     <div>
       <a href='https://github.com/JSchutza' target="_blank"> <VscGithub /> </a>
+      <a href='https://www.linkedin.com/in/joshua-schutza-559819ba/' target="_blank"> <FaLinkedin /> </a>
     </div>
   )
 };
