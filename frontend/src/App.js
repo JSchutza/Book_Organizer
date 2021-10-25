c2901928f6b5db84e6af827a0c751b27d4d1d171
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-10-25 16:15:55 -0700

    mobile-styles-for-successmessage-done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 
 
 import MainRouter from "./components/MainRouter";
-import LoadScreen from "./components/LoadScreen";
+import SuccessMessage from "./components/SuccessMessage";
 
 
 const App = () => {
